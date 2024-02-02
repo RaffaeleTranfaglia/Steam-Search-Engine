ffa31de782f05590285d0aec018b12f102628224
SamueleTondelli <samueletondelli92@gmail.com>
2024-02-02 15:15:23 +0100

    updated index storage path

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from TextUtilities.indexer import Indexer
 import GUI.MainWindow
 from MainImplementation.GameSearcher import GameSearcher
 from rich.console import Console
+import os
 
 if __name__ == "__main__":
     console = Console()
+    threads = 8
+    do_sentiment_analysis = True
+    index_dir = "indexdir/base" if not do_sentiment_analysis else "indexdir/sentiment"
+    if not os.path.exists("indexdir"):
+        os.mkdir("indexdir")
     with console.status("[bold green]Creating index...") as status:
-        main_idx, reviews_idx = Indexer.openIndex("Dataset", "indexdir", console, do_sentiment=True, worker_threads=8)
+        main_idx, reviews_idx = Indexer.openIndex("Dataset", index_dir, console, do_sentiment=do_sentiment_analysis, worker_threads=8)
     searcher = GameSearcher(main_idx)
     GUI.MainWindow.launchGui(searcher)
