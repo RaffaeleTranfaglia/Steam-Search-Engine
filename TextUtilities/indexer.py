5cae7824e6a685b065263c8d5713b9bfebf65de2
SamueleTondelli <samueletondelli92@gmail.com>
2024-02-03 13:23:03 +0100

    added another measure of sentiments based on the weighted average of all sentiments, with the weight being the inverse of the neutral score

--- TextUtilities/indexer.py
+++ TextUtilities/indexer.py
@@ -44,51 +44,118 @@ class GameDocument:
             self.minimum_requirements = game_data["minimum_requirements"]
             self.recommended_requirements = game_data["recommended_requirements"]
             self.reviews = []
-            self.anger = -1
-            self.disgust = -1
-            self.fear = -1
-            self.joy = -1
-            self.neutral = -1
-            self.sadness = -1
-            self.surprise = -1
+            self.av_anger = -1
+            self.av_disgust = -1
+            self.av_fear = -1
+            self.av_joy = -1
+            self.av_neutral = -1
+            self.av_sadness = -1
+            self.av_surprise = -1
+            self.inav_anger = -1
+            self.inav_disgust = -1
+            self.inav_fear = -1
+            self.inav_joy = -1
+            self.inav_neutral = -1
+            self.inav_sadness = -1
+            self.inav_surprise = -1
             for r in game_data["reviews"]:
                 self.reviews.append(ReviewDocument(r["review_text"], r["review_score"]))
 
             if do_sentiment and classifier is not None:
-                self.anger = 0
-                self.disgust = 0
-                self.fear = 0
-                self.joy = 0
-                self.neutral = 0
-                self.sadness = 0
-                self.surprise = 0
+                self.av_anger = 0
+                self.av_disgust = 0
+                self.av_fear = 0
+                self.av_joy = 0
+                self.av_neutral = 0
+                self.av_sadness = 0
+                self.av_surprise = 0
+                self.inav_anger = 0
+                self.inav_disgust = 0
+                self.inav_fear = 0
+                self.inav_joy = 0
+                self.inav_neutral = 0
+                self.inav_sadness = 0
+                self.inav_surprise = 0
+                inav_weights_sum = 0
                 for r in self.reviews:
+                    neutral = 0
+                    anger = 0
+                    disgust = 0
+                    fear = 0
+                    joy = 0
+                    sadness = 0
+                    surprise = 0
                     for s in r.doSentimentAnalysis(classifier):
                         s_type = s["label"]
                         if s_type == "anger":
-                            self.anger += s["score"]
+                            anger = s["score"]
                         elif s_type == "disgust":
-                            self.disgust += s["score"]
+                            disgust = s["score"]
                         elif s_type == "fear":
-                            self.fear += s["score"]
+                            fear = s["score"]
                         elif s_type == "joy":
-                            self.joy += s["score"]
+                            joy = s["score"]
                         elif s_type == "neutral":
-                            self.neutral += s["score"]
+                            neutral = s["score"]
                         elif s_type == "sadness":
-                            self.sadness += s["score"]
+                            sadness = s["score"]
                         elif s_type == "surprise":
-                            self.surprise += s["score"]
-                n_reviews = len(self.reviews) if len(self.reviews) > 0 else 1
-                self.anger /= n_reviews
-                self.disgust /= n_reviews
-                self.fear /= n_reviews
-                self.joy /= n_reviews
-                self.neutral /= n_reviews
-                self.sadness /= n_reviews
-                self.surprise /= n_reviews
+                            surprise = s["score"]
 
+                    self.av_anger += anger
+                    self.av_disgust += disgust
+                    self.av_fear += fear
+                    self.av_joy += joy
+                    self.av_neutral += neutral
+                    self.av_sadness += sadness
+                    self.av_surprise += surprise
+
+                    #to ensure to not divide by 0
+                    if neutral == 0:
+                        neutral = 0.0000000001
+
+                    self.inav_anger += (1 / neutral) * anger
+                    self.inav_disgust += (1 / neutral) * disgust
+                    self.inav_fear += (1 / neutral) * fear
+                    self.inav_joy += (1 / neutral) * joy
+                    self.inav_neutral += (1 / neutral) * neutral
+                    self.inav_sadness += (1 / neutral) * sadness
+                    self.inav_surprise += (1 / neutral) * surprise
+                    inav_weights_sum += (1 / neutral)
+
+                n_reviews = len(self.reviews) if len(self.reviews) > 0 else 1
+                self.av_anger /= n_reviews
+                self.av_disgust /= n_reviews
+                self.av_fear /= n_reviews
+                self.av_joy /= n_reviews
+                self.av_neutral /= n_reviews
+                self.av_sadness /= n_reviews
+                self.av_surprise /= n_reviews
+                self.inav_anger /= inav_weights_sum
+                self.inav_disgust /= inav_weights_sum
+                self.inav_fear /= inav_weights_sum
+                self.inav_joy /= inav_weights_sum
+                self.inav_neutral /= inav_weights_sum
+                self.inav_sadness /= inav_weights_sum
+                self.inav_surprise /= inav_weights_sum
         print(f"finished loading {self.app_id}")
+
+    def printSentiments(self):
+        print(f"av_anger : {self.av_anger}")
+        print(f"av_disgust : {self.av_disgust}")
+        print(f"av_fear : {self.av_fear}")
+        print(f"av_joy : {self.av_joy}")
+        print(f"av_neutral : {self.av_neutral}")
+        print(f"av_sadness : {self.av_sadness}")
+        print(f"av_surprise : {self.av_surprise}")
+        print("-----------------------------------")
+        print(f"inav_anger : {self.inav_anger}")
+        print(f"inav_disgust : {self.inav_disgust}")
+        print(f"inav_fear : {self.inav_fear}")
+        print(f"inav_joy : {self.inav_joy}")
+        print(f"inav_neutral : {self.inav_neutral}")
+        print(f"inav_sadness : {self.inav_sadness}")
+        print(f"inav_surprise : {self.inav_surprise}")
 
 
 def createGameDocument(file_path, do_sentiment=False, classifier=None):
@@ -134,14 +201,20 @@ class Indexer:
                         description=TEXT(stored=True, analyzer=customAnalyzer),
                         header_img=STORED,
                         minimum_requirements=STORED, recommended_requirements=STORED,
-                        anger=NUMERIC(stored=True),
-                        disgust=NUMERIC(stored=True),
-                        fear=NUMERIC(stored=True),
-                        joy=NUMERIC(stored=True),
-                        neutral=NUMERIC(stored=True),
-                        sadness=NUMERIC(stored=True),
-                        surprise=NUMERIC(stored=True)
-                        )
+                        av_anger=NUMERIC(stored=True),
+                        av_disgust=NUMERIC(stored=True),
+                        av_fear=NUMERIC(stored=True),
+                        av_joy=NUMERIC(stored=True),
+                        av_neutral=NUMERIC(stored=True),
+                        av_sadness=NUMERIC(stored=True),
+                        av_surprise=NUMERIC(stored=True),
+                        inav_anger=NUMERIC(stored=True),
+                        inav_disgust=NUMERIC(stored=True),
+                        inav_fear=NUMERIC(stored=True),
+                        inav_joy=NUMERIC(stored=True),
+                        inav_neutral=NUMERIC(stored=True),
+                        inav_sadness=NUMERIC(stored=True),
+                        inav_surprise=NUMERIC(stored=True))
         
         main_idx = create_in(folder_index + "/main_index", main_schema)
         main_writer = main_idx.writer()
@@ -187,13 +260,20 @@ class Indexer:
                                      header_img=g.header_img,
                                      minimum_requirements=g.minimum_requirements,
                                      recommended_requirements=g.recommended_requirements,
-                                     anger=g.anger,
-                                     disgust=g.disgust,
-                                     fear=g.fear,
-                                     joy=g.joy,
-                                     neutral=g.neutral,
-                                     sadness=g.sadness,
-                                     surprise=g.surprise)
+                                     av_anger=g.av_anger,
+                                     av_disgust=g.av_disgust,
+                                     av_fear=g.av_fear,
+                                     av_joy=g.av_joy,
+                                     av_neutral=g.av_neutral,
+                                     av_sadness=g.av_sadness,
+                                     av_surprise=g.av_surprise,
+                                     inav_anger=g.inav_anger,
+                                     inav_disgust=g.inav_disgust,
+                                     inav_fear=g.inav_fear,
+                                     inav_joy=g.inav_joy,
+                                     inav_neutral=g.inav_neutral,
+                                     inav_sadness=g.inav_sadness,
+                                     inav_surprise=g.inav_surprise)
 
             for r in g.reviews:
                 reviews_writer.add_document(app_id=g.app_id,
