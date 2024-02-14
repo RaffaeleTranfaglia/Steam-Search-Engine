b4b653ff25efb187235924e95ea5a8e4cb149687
SamueleTondelli <samueletondelli92@gmail.com>
2024-02-14 15:55:17 +0100

    updated comments

--- MainImplementation/GameSearcher.py
+++ MainImplementation/GameSearcher.py
@@ -7,11 +7,15 @@ from transformers import pipeline
 import re
 from typing import List, Tuple
 from whoosh.searching import Hit
-import gensim
-from gensim.models.doc2vec import Doc2Vec
 from TextUtilities.analyzer import TokenAnalyzer
 
 
+'''
+    Computes the cosine similarity between the two 7d sentiment vectors sv1 and sv2
+    
+    @param sv1: vector 1
+    @param sv2: vector 2
+'''
 def cosineSimilarity(sv1, sv2):
     dot_p = 0
     mod_sv1 = 0
@@ -24,7 +28,20 @@ def cosineSimilarity(sv1, sv2):
     return dot_p / (math.sqrt(mod_sv1) * math.sqrt(mod_sv2)) if mod_sv1 != 0 and mod_sv2 != 0 else 0
 
 
+# Class which executes queries
 class GameSearcher:
+    '''
+        Constructor
+
+        @param main_idx: main games' index
+        @param reviews_idx: reviews index
+        @param do_sentiment: enables sentiment analysis
+        @param sentiment_version: which version used to rank games with sentiment analysis, must be either "false", "av" or "inav"
+        @param d2v: enables Doc2Vec
+        @param d2v_models: dict with the Doc2Vec models
+        @param d2v_i_to_fp: dict to translate game index in the Doc2Vec model to game filepath
+        @param dataset_path: path to the dataset, only used if Doc2Vec is enabled
+    '''
     def __init__(self, main_idx, reviews_idx, do_sentiment=False, sentiment_version=None, d2v=False, d2v_models=None, d2v_i_to_fp=None, dataset_path=None):
         self.main_idx = main_idx
         self.reviews_idx = reviews_idx
@@ -38,12 +55,22 @@ class GameSearcher:
         self.d2v_i_to_fp = d2v_i_to_fp
         self.dataset_path = dataset_path
 
+    '''
+        Executes the query queryText on fields, limiting the results at limit using the appropriate version
+        
+        @param queryText: text of the query
+        @param fields: fields to search on
+        @param limit: max number of results to the query
+    '''
     def search(self, queryText: str, fields, limit=10):
         if self.d2v:
             return self.searchD2V(queryText, fields, limit)
+
+        # not doing doc2vec
         searcher = self.main_idx.searcher()
         parser = MultifieldParser(fields, self.main_idx.schema)
 
+        # searches for the sentiment part of the query
         match = re.search(r'sentiment\[(.*?)\]', queryText)
         qsentv = []
         sent_prefix = ""
@@ -79,8 +106,10 @@ class GameSearcher:
                     elif s_type == "surprise":
                         surprise = s["score"]
 
+                # sentiment vector of the query
                 qsentv = [anger, disgust, fear, joy, neutral, sadness, surprise]
 
+                # chooses right measure of sentiment based on the constructor param
                 sent_prefix = "inav_"
                 if self.sentiment_version == "av":
                     sent_prefix = "av_"
@@ -99,6 +128,8 @@ class GameSearcher:
                 if revs <= 1:
                     revs = 2
 
+                # computes the similarity between the sentiment of the query and each game, the similarity is multiplied
+                # by a weight function which increases with more ratings, with a higher k this effect is amplified
                 k = 400
                 result_order.append((g, cosineSimilarity(qsentv, gsentv) * math.exp(-k/(revs*math.log(revs, 10)))))
 
@@ -109,6 +140,12 @@ class GameSearcher:
 
             return result[:limit]
 
+    '''
+        Returns the list of reviews of a particular game
+        
+        @param app_id: app_id of the game
+        @param limit: max number of reviews returned
+    '''
     def getGameReviews(self, app_id, limit=None):
         if self.d2v:
             reviews = []
@@ -122,7 +159,13 @@ class GameSearcher:
         query = parser.parse(app_id)
         return searcher.search(query, limit=limit)
 
-
+    '''
+        Searches most similar games to vector q on a single field with max results n
+        
+        @param q: query vector
+        @param n: length of returned games
+        @param field: field to search on
+    '''
     def searchD2VSingleField(self, q, n, field):
         qv = self.d2v_models[field].infer_vector(q)
         sims = self.d2v_models[field].dv.most_similar([qv], topn=n)
@@ -131,10 +174,20 @@ class GameSearcher:
             r.append((self.d2v_i_to_fp[s[0]], s[1]))
         return r
 
+    '''
+        Executes the query queryText on fields, limiting the results at limit using the Doc2Vec version
+        
+        @param queryText: text of the query
+        @param fields: fields to search on
+        @param limit: max number of results to the query
+    '''
     def searchD2V(self, queryText: str, fields, limit=10):
+        # removes the sentiment part of the query if present
         match = re.search(r'sentiment\[(.*?)\]', queryText)
         if match:
             queryText = re.sub(r'\\sentiment\[(.*?)\]', '', queryText)
+
+        # preprocess the query and search it in every field
         qv = TokenAnalyzer.preprocessing(queryText)
         singular_results = []
         for f in fields:
@@ -142,6 +195,7 @@ class GameSearcher:
             for r in rs:
                 singular_results.append({"app_id": r[0], "sim": r[1]})
 
+        # computes the average similarity of each game with the query
         appids = set([r["app_id"] for r in singular_results])
         appids_sims = []
         for a in appids:
@@ -151,7 +205,10 @@ class GameSearcher:
                     s += r["sim"]
             s /= len(fields)
             appids_sims.append((a, s))
+
         appids_sims.sort(key=lambda a: a[1], reverse=True)
+
+        # reads game data of each relevant game
         result = []
         for f in appids_sims[:limit]:
             with open(f[0], 'r', encoding='utf-8') as game_file:
